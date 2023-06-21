59eb667edde0c7738b938c0cf8730297cf1e0c84
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-06-21 18:44:28 +0530

    Changed Connection Mode

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask,jsonify
 import json
-from DataScrape import GetProvinces
+from flask import request
 from Database import SelectQuery
 
 app = Flask(__name__)
 
-@app.route("/provinces")
+@app.route("/provinces",methods=["GET"])
 def ProvincesList():
     provinces = SelectQuery("SELECT * FROM provinces")
     print(json.dumps({"provinces":provinces}))
     return json.dumps(provinces)
 
-@app.route("/districts")
+@app.route("/districts",methods=["GET"])
 def DistrictsList():
     districts= SelectQuery("SELECT id,name FROM districts")
     return json.dumps(districts)
 
-@app.route("/divisions")
+@app.route("/districts",methods=["POST"])
+def DistrictsListPOST():
+    dataset = json.dumps(request.data)
+    print(dataset)
+    districts = SelectQuery(f"SELECT id,name FROM districts WHERE province_id = {dataset['province']}")
+    print(districts)
+    return districts
+
+@app.route("/divisions",methods=["GET"])
 def DivisionsList():
     divisions= SelectQuery("SELECT id,name FROM divisions")
     return json.dumps(divisions)
