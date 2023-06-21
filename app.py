d3c065f7af6b9bb9dd4543607a3eaa2878ba48d1
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-06-21 20:58:59 +0530

    Added Readme and Homepage

--- app.py
+++ app.py
@@ -5,6 +5,91 @@ from Database import SelectQuery
 
 app = Flask(__name__)
 
+@app.route("/")
+def Home():
+    return '''<h1 id="sri-lanka-administrative-data-api">Sri Lanka Administrative Data API</h1>
+<p>👋 Welcome to the Sri Lanka Administrative Data API repository! This API provides access to provinces, districts, and division data in Sri Lanka.</p>
+<h2 id="installation">Installation</h2>
+<ol>
+<li>Clone the repository:</li>
+</ol>
+<pre><code class="lang-bash">git <span class="hljs-keyword">clone</span> <span class="hljs-title">https</span>://github.com/sudeepaviraj/gn-extract.git
+</code></pre>
+<ol>
+<li>Navigate to the project directory:</li>
+</ol>
+<pre><code class="lang-bash"><span class="hljs-built_in">cd</span> gn-extract
+</code></pre>
+<ol>
+<li>Install the required dependencies:</li>
+</ol>
+<pre><code class="lang-bash">pip <span class="hljs-keyword">install</span> -r requirements.txt
+</code></pre>
+<h2 id="usage">Usage</h2>
+<ol>
+<li>Run the application:</li>
+</ol>
+<pre><code class="lang-bash"><span class="hljs-keyword">python</span> app.<span class="hljs-keyword">py</span>
+</code></pre>
+<ol>
+<li><p>Once the application is running, you can access the API endpoints:</p>
+</li>
+<li><p><strong>Get all provinces</strong>: <code>/provinces</code></p>
+</li>
+<li><strong>Get all districts</strong>: <code>/districts</code></li>
+<li><strong>Get all divisions</strong>: <code>/divisions</code></li>
+<li><strong>Get all districts in a province</strong>: <code>/getdistricts?province={province_id}</code></li>
+<li><p><strong>Get all divisions in a district</strong>: <code>/getdivisons?district={district_id}</code></p>
+</li>
+<li><p>Make HTTP GET requests to the desired endpoints using tools like <code>curl</code> or <code>Postman</code> to retrieve the administrative data.</p>
+</li>
+</ol>
+<p>Example request using <code>curl</code>:</p>
+<pre><code class="lang-bash">curl <span class="hljs-string">http:</span><span class="hljs-comment">//localhost:5000/provinces</span>
+</code></pre>
+<p>Example response:</p>
+<pre><code class="lang-json">[
+    {
+        <span class="hljs-attr">"id"</span>: <span class="hljs-number">1</span>,
+        <span class="hljs-attr">"name"</span>: <span class="hljs-string">"Western"</span>
+    },
+    {
+        <span class="hljs-attr">"id"</span>: <span class="hljs-number">2</span>,
+        <span class="hljs-attr">"name"</span>: <span class="hljs-string">"Central"</span>
+    },
+    ...
+]
+</code></pre>
+<h2 id="demo-api">Demo API</h2>
+<p>You can access the demo of this project from</p>
+<pre><code><span class="hljs-symbol">http:</span><span class="hljs-comment">//db.famed.cloud/</span>
+</code></pre><h2 id="contributing">Contributing</h2>
+<p>We welcome contributions to improve this project! If you&#39;d like to contribute, please follow these steps:</p>
+<ol>
+<li><p>Fork the repository.</p>
+</li>
+<li><p>Create a new branch for your feature:</p>
+</li>
+</ol>
+<pre><code class="lang-bash">git checkout -b <span class="hljs-built_in">feature</span>/your-<span class="hljs-built_in">feature</span>-name
+</code></pre>
+<ol>
+<li>Make your changes and commit them:</li>
+</ol>
+<pre><code class="lang-bash"><span class="hljs-attribute">git</span> commit -m <span class="hljs-string">"Add your commit message"</span>
+</code></pre>
+<ol>
+<li>Push your changes to your forked repository:</li>
+</ol>
+<pre><code class="lang-bash">git <span class="hljs-built_in">push</span> <span class="hljs-built_in">origin</span> <span class="hljs-built_in">feature</span>/your-<span class="hljs-built_in">feature</span>-name
+</code></pre>
+<ol>
+<li>Open a pull request, and we&#39;ll review your changes.</li>
+</ol>
+<h2 id="license">License</h2>
+<p>This project is licensed under the <a href="LICENSE">MIT License</a>.</p>
+'''
+
 @app.route("/provinces",methods=["GET"])
 def ProvincesList():
     provinces = SelectQuery("SELECT * FROM provinces")
---
+++ readme.md
@@ -0,0 +1,101 @@
+# Sri Lanka Administrative Data API
+
+👋 Welcome to the Sri Lanka Administrative Data API repository! This API provides access to provinces, districts, and division data in Sri Lanka.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/sudeepaviraj/gn-extract.git
+```
+
+2. Navigate to the project directory:
+
+```bash
+cd gn-extract
+```
+
+3. Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+1. Run the application:
+
+```bash
+python app.py
+```
+
+2. Once the application is running, you can access the API endpoints:
+
+- **Get all provinces**: `/provinces`
+- **Get all districts**: `/districts`
+- **Get all divisions**: `/divisions`
+- **Get all districts in a province**: `/getdistricts?province={province_id}`
+- **Get all divisions in a district**: `/getdivisons?district={district_id}`
+
+3. Make HTTP GET requests to the desired endpoints using tools like `curl` or `Postman` to retrieve the administrative data.
+
+Example request using `curl`:
+
+```bash
+curl http://localhost:5000/provinces
+```
+
+Example response:
+
+```json
+[
+    {
+        "id": 1,
+        "name": "Western"
+    },
+    {
+        "id": 2,
+        "name": "Central"
+    },
+    ...
+]
+```
+
+## Demo API
+
+You can access the demo of this project from
+
+```
+http://db.famed.cloud/
+```
+
+## Contributing
+
+We welcome contributions to improve this project! If you'd like to contribute, please follow these steps:
+
+1. Fork the repository.
+
+2. Create a new branch for your feature:
+
+```bash
+git checkout -b feature/your-feature-name
+```
+
+3. Make your changes and commit them:
+
+```bash
+git commit -m "Add your commit message"
+```
+
+4. Push your changes to your forked repository:
+
+```bash
+git push origin feature/your-feature-name
+```
+
+5. Open a pull request, and we'll review your changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
